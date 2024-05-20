d4f694f10dcc2a437d284ed011ac1511f2c224a5
GitHub Action <action@github.com>
2024-05-20 06:50:42 +0000

    Initialize template repository proper
    Set package name to com.github.akash11107.modsforyou
    Set project name to ModsForYou
    Set modid to modsforyou

--- src/main/java/com/github/akash11107/modsforyou/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/akash11107/modsforyou/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.akash11107.modsforyou.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
